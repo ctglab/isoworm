f714b6a9035f4448ec56f40362022c33e91f8361
Maurizio Salvatore Podda <m.podda@student.unisi.it>
2024-05-22 17:45:18 +0200

    test

--- workflow/util/script.sh
+++ workflow/util/script.sh
@@ -1,2 +1,6 @@
+ls
+ls results/
+ls results//fastq
 cd results/fastq/$1
-ffq --ftp $1 | grep -Eo '"url": "[^"]*"' | grep -o ftp.sra.*$ | sed 's/.$//' | xargs curl -O -C - -o nul -#
+ffq --ftp $1 | grep -Eo '"url": "[^"]*"' | grep -o ftp.sra.*$ | sed 's/.$//' | xargs curl -O -C - -o nul
+ls
