3ae74795e07ecfa35fb91230674168281aa596e4
Maurizio Salvatore Podda <91120937+mspodda@users.noreply.github.com>
2025-07-16 11:36:22 +0200

    Update script.sh

--- workflow/util/script.sh
+++ workflow/util/script.sh
@@ -1,3 +1,3 @@
-cd results/lung_geo/fastq/$1 # Change to the directory where the FASTQ files will be downloaded
+cd results/lung_geo/fastq/$1 # Change only this line, the directory where the FASTQ files will be downloaded
 # Download FASTQ files from the SRA using the provided accession number
 ffq --ftp $1 | grep -Eo '"url": "[^"]*"' | grep -o ftp.sra.*$ | sed 's/.$//' | xargs -n 1 curl -O -C - --silent --show-error
